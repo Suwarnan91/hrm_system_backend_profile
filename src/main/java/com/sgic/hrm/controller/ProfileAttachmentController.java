ec8174683fec91669fa4ed2c982cb630d9626dfb
paki <tpakikaran14@gmail.com>
2018-11-20 15:14:26 +0530

    user complete

---
+++ src/main/java/com/sgic/hrm/controller/ProfileAttachmentController.java
@@ -0,0 +1,10 @@
+package com.sgic.hrm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileAttachmentController {
+	
+	
+}
